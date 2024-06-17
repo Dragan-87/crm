77dd7679643dcb27d0c3676f319973ce4264fb96
Dragan <illutas1987@gmail.com>
2024-06-17 14:42:45 +0200

    refactor: Update toolbar component to include a responsive sidebar
    The toolbar component has been updated to include a responsive sidebar using the Angular Material `mat-drawer` component. This allows users to toggle the sidebar by clicking on a menu icon. The sidebar is now auto-resizing and takes up the full height of the viewport.
    
    This commit also includes updates to the toolbar component's HTML, SCSS, and TypeScript files to import the necessary Angular Material modules.

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
 
+
+  openSidebar() {
+    console.log('open sidebar');
+  }
 }
