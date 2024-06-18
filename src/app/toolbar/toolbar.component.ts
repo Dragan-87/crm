673b059595acbee7fe948c3be9ef23c6864c7215
Dragan <illutas1987@gmail.com>
2024-06-18 11:39:06 +0200

    feat: Add responsive sidebar to toolbar component

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -3,12 +3,15 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, RouterOutlet, RouterLink],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
