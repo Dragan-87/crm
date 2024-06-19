323fcbeb2b5384bb1ea27b9a5c56b8f2892b4ae6
Dragan <illutas1987@gmail.com>
2024-06-19 10:42:13 +0200

    feat: Update toolbar component to include responsive sidebar
    The toolbar component has been updated to include a responsive sidebar using the Angular Material `mat-drawer` component. This allows users to toggle the sidebar by clicking on a menu icon. The sidebar is now auto-resizing and takes up the full height of the viewport.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { UserDialogComponent } from './user-dialog/user-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, UserDialogComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
 
+  constructor(private dialog: MatDialog) {
+    
+  }
+
+  openDialog() {
+    this.dialog.open(UserDialogComponent);
+  }
 }
