ed68ba0bfe31ed8fb46ab203990da2c3b86a1f4a
Dragan <illutas1987@gmail.com>
2024-06-24 10:11:09 +0200

    feat: Update user-dialog component to include email form
    The user-dialog component has been updated to include a form for user email input. This change allows users to enter their email address and displays error messages for invalid or missing email addresses.

--- src/app/user/user-dialog/user-dialog.component.ts
+++ src/app/user/user-dialog/user-dialog.component.ts
@@ -4,14 +4,13 @@ import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
-  Validators,
-  FormsModule,
+  Validators
 } from '@angular/forms';
 
 @Component({
   selector: 'app-user-dialog',
   standalone: true,
-  imports: [MatInputModule, FormsModule, ReactiveFormsModule],
+  imports: [MatInputModule, ReactiveFormsModule],
   templateUrl: './user-dialog.component.html',
   styleUrl: './user-dialog.component.scss'
 })
@@ -22,7 +21,10 @@ export class UserDialogComponent {
 
   constructor() {
     this.userFormGroup = new FormGroup({
+      fistname: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-ZäöüÄÖß]{3,}$')]),
+      lastname: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-ZäöüÄÖß]{3,}$')]),
       email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(3), Validators.pattern('^[a-zA-ZäöüÄÖß0-9]+(\.[a-zA-ZäöüÄÖß0-9]+)*@[a-zA-ZäöüÄÖß0-9]+\.[a-zA-Z]{2,}$')]),
+      birthdate: new FormControl('', [Validators.required, Validators.pattern('^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.[0-9]{4}$')]),
     });
   }
 
